dd01c8f7b249a9754700d1e1dcae6d22ea8cac70
kkwik <kevinkwik6@gmail.com>
2022-11-17 13:43:45 -0800

    cleanup

--- agents/our.py
+++ agents/our.py
@@ -16,8 +16,6 @@ class OurPlayer(agent.Agent):
         self.pnr = pnr
         self.explanation = []
     def get_action(self, nr, hands, knowledge, trash, played, board, valid_actions, hints, hits, cards_left):
-        # print(f'====ACTION====')
-        # print(f'board: {board}')
         for player,hand in enumerate(hands):
             for card_index,_ in enumerate(hand):
                 if (player,card_index) not in self.hints:
@@ -29,8 +27,7 @@ class OurPlayer(agent.Agent):
                 known[card_index] = str(list(map(format_hint, self.hints[h])))
         self.explanation = [["hints received:"] + known]
 
-
-        
+        # Figure out what cards we want and don't want
         playable_cards = [Card(color, curr_rank+1) for color, curr_rank in board if curr_rank+1 <= 5] # Cards that are currently playable on the board
         unplayable_cards = []                                                                          # Cards that are not playable on the board
         for card in board:
@@ -39,29 +36,24 @@ class OurPlayer(agent.Agent):
                 unplayable_cards.append(Card(card_color, rank+1))
 
         my_knowledge = knowledge[nr]
+
         my_informed_knowledge = copy.deepcopy(my_knowledge) # Knowledge informed by discard pile and other players hands
-        
         # Adjust my_knowledge based on other knowledge
         for i,k in enumerate(my_informed_knowledge):
             # Discount stuff we've discarded
             for discarded_card in trash:
                 if k[discarded_card[0]][discarded_card[1] - 1] > 0:
-                    # print(' discount from trash')
                     k[discarded_card[0]][discarded_card[1] - 1] -= 1
 
             # Discount stuff we can see in hands
-            # print(f'len of hands {len(hands)}')
             for player_hand in hands:
                 for player_card in player_hand:
-                    # print(f'len of player_hand {len(player_hand)}')
                     if k[player_card[0]][player_card[1] - 1] > 0:
-                        # print(f' discount from hand')
                         k[player_card[0]][player_card[1] - 1] -= 1
             
             # Discount stuff we can see on the board
             for played_card in unplayable_cards:
                 if k[played_card.color][played_card.rank - 1] > 0:
-                    # print(' discount from board')
                     k[played_card.color][played_card.rank - 1] -= 1
             
             # Save adjusted
@@ -71,33 +63,26 @@ class OurPlayer(agent.Agent):
         probably_playable = []
         potential_discards = []
         for i,k in enumerate(my_informed_knowledge):
-        
             if util.is_playable(k, board):
                 self.explanation.append("dead set on this card")
                 return Action(PLAY, card_index=i) # Step 1
             if util.is_useless(k, board):    
                 potential_discards.append(i)
-            # print(f'Card number {i}')
+
             for wanted_card in playable_cards:
-                # print(f'    {wanted_card}s in play: {k[wanted_card.color][wanted_card.rank - 1]} not accounted for by discard or visible')
-                # print(f'    card {i} is {wanted_card} with {util.probability_is_card(k, wanted_card)} probability.')
+                # If this current card is likely a card we want, add it to probably_playable
                 if util.probability_is_card(k, wanted_card) > 0.55: # PARAMETER we can mess with
-                    # print(f'card {i} is {wanted_card} with {util.probability_is_card(k, wanted_card)} probability.')
                     probably_playable.append((util.probability_is_card(k, wanted_card), i, wanted_card))
         
         # If there are any cards in our hand we are reasonably certain are playable, play the best one
         if len(probably_playable) > 0:
-            
             probably_playable.sort(key=lambda x: x[0], reverse=True) # Sort by most probable to least
             most_likely = probably_playable[0]
-            self.explanation.append(f'me thinks its {most_likely[2]} with a {most_likely[0]}% chance')
             return Action(PLAY, card_index=most_likely[1])
 
         # Step 2
         if potential_discards:
-            # print(f'Potential discards: {potential_discards}')
             return Action(DISCARD, card_index=random.choice(potential_discards))
-         
 
         # Step 3
         playables = []        
@@ -156,7 +141,7 @@ class OurPlayer(agent.Agent):
         # Step 5
           
         probably_discardable = []
-        # print(f'trash: {trash}')
+        # This part isn't actually helping, might have to investigate more later
         # for i,k in enumerate(my_informed_knowledge):
         #     for unwanted_card in unplayable_cards:
         #         # if util.probability_is_card(k, unwanted_card) > 0.01: # PARAMETER we can mess with
